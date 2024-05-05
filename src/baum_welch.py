bf13f48396b665773cd106f3e05ecff50b39a74d
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-06 00:34:42 +0300

    only difference

--- src/baum_welch.py
+++ src/baum_welch.py
@@ -24,22 +24,39 @@ def serialize_country_data(country: Country):
     while getlist(CPI)[0] != getlist(GDP)[0] or getlist(CPI)[0] != getlist(IR)[0]:
         if getlist(CPI)[0] < getlist(GDP)[0] and getlist(CPI)[0] < getlist(IR)[0]:
             getlist(CPI).pop(0)
+            country_indicators[CPI]['values'].pop(0)
         elif getlist(IR)[0] < getlist(GDP)[0]:
             getlist(IR).pop(0)
+            country_indicators[IR]['values'].pop(0)
         else: 
             getlist(GDP).pop(0)
+            country_indicators[GDP]['values'].pop(0)
 
     while getlist(CPI)[-1] != getlist(GDP)[-1] or getlist(CPI)[-1] != getlist(IR)[-1]:
         if getlist(CPI)[-1] > getlist(GDP)[-1] and getlist(CPI)[-1] > getlist(IR)[-1]:
             getlist(CPI).pop()
+            country_indicators[CPI]['values'].pop()
         elif getlist(IR)[-1] > getlist(GDP)[-1]:
             getlist(IR).pop()
+            country_indicators[IR]['values'].pop()
         else: 
             getlist(GDP).pop()
+            country_indicators[GDP]['values'].pop()
       
+    length = len(country_indicators[GDP]['years'])
     country_data: dict[Indicator, list[float]] = {}
     for indicator in Indicator:
         country_data[indicator] = country_indicators[indicator]['values']
+        if len(country_data[indicator]) != length:
+            raise Exception(f"{country} has different lengths of indicator even if same start and end date")
+    
+    for i in range(length):
+        if i == length - 1: continue
+        country_data[CPI][length - i - 1] = country_data[CPI][length - i - 1] - country_data[CPI][length - i - 2]
+        country_data[IR][length - i - 1] = country_data[IR][length - i - 1] - country_data[IR][length - i - 2]
+
+    for indicator in Indicator:
+        country_data[indicator].pop(0)
     return country_data
     
       
@@ -54,7 +71,7 @@ if __name__ == "__main__":
 
     for country in Country:
       countries_data[country] = serialize_country_data(country)
-    print(countries_data)
+    print(countries_data[Country.BRAZIL])
 
     
 
