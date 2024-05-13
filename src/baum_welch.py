7de476a7bd5fa954c71e40871c5e6f565f49c386
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-13 16:50:43 +0300

    construct the starting markov chains from the country data

--- src/baum_welch.py
+++ src/baum_welch.py
@@ -10,6 +10,9 @@ from src.models import MarkovChain
 GDP, IR, CPI = Indicator
 
 class HiddenState(Enum):
+    '''
+        The hidden state of the markov chain. Each state tells if the Interest Rate and the CPI are increasing or decreasing
+    '''
     I_IR_I_CPI = 0
     I_IR_D_CPI = 1
     D_IR_I_CPI = 2
@@ -19,13 +22,31 @@ class HiddenState(Enum):
     def get_all_states() -> list[int]:
         return [state.value for state in HiddenState]
     
+    @staticmethod
+    def get_state(ir: float, cpi: float):
+        if ir > 0 and cpi > 0:
+            return HiddenState.I_IR_I_CPI
+        elif ir > 0 and cpi < 0:
+            return HiddenState.I_IR_D_CPI
+        elif ir < 0 and cpi > 0:
+            return HiddenState.D_IR_I_CPI
+        else:
+            return HiddenState.D_IR_D_CPI
+    
 class KnownVariables(Enum):
+    '''
+        The known variables of the markov chain. Each state tells if the GDP is increasing or decreasing
+    '''
     I_GDP = 0
     D_GDP = 1
 
     @staticmethod
     def get_all_variables() -> list[int]:
         return [var.value for var in KnownVariables]
+    
+    @staticmethod
+    def get_variable(gdp: float):
+        return KnownVariables.I_GDP if gdp > 0 else KnownVariables.D_GDP
 
 def serialize_country_data(country: Country):
     '''
@@ -93,7 +114,52 @@ def serialize_country_data(country: Country):
             country_data[indicator][i] = 1 if country_data[indicator][i] >= 0 else -1
     return country_data
     
-      
+def construct_starting_markov_chain(country_data: dict[Indicator, list[float]]):
+    '''
+    Construct the starting markov chain from the country data
+    '''
+    length = len(country_data[GDP])
+    hidden_states = np.zeros(4)
+    known_var_states = np.zeros(2)
+    hidden_transitions = np.zeros((4, 4))
+    known_var_transitions = np.zeros((4, 2))
+
+    # * count the number of each state and transition
+    for i in range(length):
+        hidden_state: HiddenState = HiddenState.get_state(country_data[IR][i], country_data[CPI][i])
+        known_var_state: KnownVariables = KnownVariables.get_variable(country_data[GDP][i])
+
+        hidden_states[hidden_state.value] += 1
+        known_var_states[known_var_state.value] += 1
+
+        if i == length - 1: continue
+        next_hidden_state: HiddenState = HiddenState.get_state(country_data[IR][i + 1], country_data[CPI][i + 1])
+        next_known_var_state: KnownVariables = KnownVariables.get_variable(country_data[GDP][i + 1])
+
+        hidden_transitions[hidden_state.value][next_hidden_state.value] += 1
+        known_var_transitions[hidden_state.value][next_known_var_state.value] += 1
+
+    # * normalize the probabilities
+    hidden_states = hidden_states / np.sum(hidden_states)
+    known_var_states = known_var_states / np.sum(known_var_states)
+    for i in range(4):
+        hidden_transitions[i] = hidden_transitions[i] / np.sum(hidden_transitions[i])
+        known_var_transitions[i] = known_var_transitions[i] / np.sum(known_var_transitions[i])
+
+    # * create the markov chains
+    hidden_markov_chain: MarkovChain = MarkovChain(
+        states=hidden_states, 
+        transitions=hidden_transitions, 
+        initial_state=0
+    )
+    known_var_markov_chain: MarkovChain = MarkovChain(
+        states=known_var_states, 
+        transitions=known_var_transitions, 
+        initial_state=0
+    )
+
+    return hidden_markov_chain, known_var_markov_chain
+
 
 if __name__ == "__main__":
     if os.path.exists("data/cleaned/dataset.csv"):
@@ -105,13 +171,11 @@ if __name__ == "__main__":
 
     for country in Country:
       countries_data[country] = serialize_country_data(country)
-    print(countries_data[Country.ITALY])
+    # print(countries_data[Country.ITALY])
 
-    # mc = MarkovChain(states=np.array([0.3, 0.7]), transistions=np.array([[0.7, 0.3], [0.3, 0.7]]), initial_state=0)
-    # print(mc.nsteps(2))
-    # print(mc.to_image('test'))
-
-
+    hidden_markov_chain, known_var_markov_chain = construct_starting_markov_chain(countries_data[Country.ITALY])
+    print(hidden_markov_chain)
+    print(known_var_markov_chain)
     
 
       
